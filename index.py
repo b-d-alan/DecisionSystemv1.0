5ab1305cdfe1e2a24a7f45ed5ad2da7af494c375
b-d-alan <balthasardanielo@gmail.com>
2025-12-31 18:29:41 +0530

    eisenhover matrix improvement, user input fields created and imported various modules

--- index.py
+++ index.py
@@ -1,28 +1,44 @@
 """
 a program to aid in decision-making
 """
+import csv
+import time
+import os 
 from my_python_package_custom_package import my_module
+
+
 def eisenhower_matrix(): 
     urgent = my_module.yn("is this task urgent?")
     important = my_module.yn("is this task important?")
+
     if urgent and important:
-        print("Do it now.")
+        decision = "Do it now."
     elif not urgent and important:
-        print("Schedule a time to do it.")
+        decision = "Schedule a time to do it."
         call_when = yn("would you like me to helo you schedule a time?")
         if call_when:
             when()
     elif urgent and not important:
-        print("Delegate it if possible.")
+        decision = "Delegate it if possible."
     else:
-        print("Eliminate it.")
+        decision = "Eliminate it."
+    print(decision)
+    with
+
 
 def when():
-
+    duration = int(input("how long will it take to complete this task? (mins) "))
+    
 
 def read_logs():
-
-
+    
+print("how may i help you? ")
+print("""1 - new task
+          2 - pending tasks
+          3 - read logs""")
 while True:
-    print("how may i help you? ")
-    print("""1 - """) 
+    command = input("enter corresponding number: ").strip()
+    if not command.isdigit():
+        print("invalid input, please enter a number")
+        continue
+    if command =="1":
