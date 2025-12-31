ea5b2146c9d18a1a70568b9448ed42fc87bafadb
b-d-alan <balthasardanielo@gmail.com>
2025-12-31 16:01:27 +0530

    eisenhover matrix logic completed

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
 """
 a program to aid in decision-making
 """
-
-def eisenhower_matrix():
-
+from my_python_package_custom_package import my_module
+def eisenhower_matrix(): 
+    urgent = my_module.yn("is this task urgent?")
+    important = my_module.yn("is this task important?")
+    if urgent and important:
+        print("Do it now.")
+    elif not urgent and important:
+        print("Schedule a time to do it.")
+        call_when = yn("would you like me to helo you schedule a time?")
+        if call_when:
+            when()
+    elif urgent and not important:
+        print("Delegate it if possible.")
+    else:
+        print("Eliminate it.")
 
 def when():
 
@@ -12,4 +24,5 @@ def read_logs():
 
 
 while True:
-    print("how ")
+    print("how may i help you? ")
+    print("""1 - """) 
